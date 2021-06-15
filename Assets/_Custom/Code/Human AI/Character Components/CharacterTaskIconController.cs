0e6daa0930e247c81663b23469a5e66d27ee14b9
Greg Poisson <gregory.poisson2671@gmail.com>
2021-06-14 18:58:26 -0600

    implemented task icon controller

---
+++ Assets/_Custom/Code/Human AI/Character Components/CharacterTaskIconController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Custom.Code
+{
+    public class CharacterTaskIconController : MonoBehaviour
+    {
+        private Camera currentCamera;
+        private List<CharacterAI> humanCharacters;
+
+        public void Start()
+        {
+            humanCharacters = new List<CharacterAI>();
+            AddCharacterIfPresent("Graham");
+            AddCharacterIfPresent("Lorraine");
+            currentCamera = Camera.main;
+        }
+
+        public void Update()
+        {
+            UpdateAllTaskIcons();
+        }
+
+        private void AddCharacterIfPresent(string characterName)
+        {
+            try
+            {
+                humanCharacters.Add(GameObject.Find(characterName).GetComponent<CharacterAI>());
+            }
+            catch (NullReferenceException e)
+            {
+            }
+        }
+
+        private void UpdateAllTaskIcons()
+        {
+            foreach (CharacterAI character in humanCharacters)
+            {
+                float characterDistanceFromCamera =
+                    Vector3.Distance(character.transform.position, currentCamera.transform.position);
+                float taskIconAdjustmentForDistance = characterDistanceFromCamera / 100f;
+                character.taskIcon.position =
+                    currentCamera.WorldToScreenPoint(character.taskIconTrackingTransform.position -
+                                                     new Vector3(0f, taskIconAdjustmentForDistance, 0f));
+            }
+        }
+    }
+}
